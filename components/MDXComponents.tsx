31cbf713b650a18e3877241c2f17957a0f2b0246
Daniel Demmel <daniel.demmel@beryl.cc>
2022-12-18 21:33:29 +0000

    Fixes

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -18,7 +18,7 @@ export const EmbedPDF = ({ URL }) => (
   <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
     <iframe
       title={URL}
-      src={`http://docs.google.com/gview?url=${URL}&embedded=true`}
+      src={`https://docs.google.com/gview?url=${URL}&embedded=true`}
       style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
     ></iframe>
   </div>
